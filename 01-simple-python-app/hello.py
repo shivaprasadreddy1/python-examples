aa91f65154626351e30c47bd8bb4e69e89c93ab7
Shiva <shivaprasadreddy1>
2017-04-18 20:32:13 -0400

    Changed message in hello_world method

--- 01-simple-python-app/hello.py
+++ 01-simple-python-app/hello.py
@@ -15,7 +15,7 @@ port = int(os.getenv("PORT", 9099))
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! I am instance ' + str(os.getenv("CF_INSTANCE_INDEX", 0))
+    return 'Shruthi20 Hello World! I am instance ' + str(os.getenv("CF_INSTANCE_INDEX", 0))
 
 if __name__ == '__main__':
     # Run the app, listening on all IPs with our chosen port number
